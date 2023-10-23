7b032385bc68d455f8f191092529e323e11623c2
Chloe Chen <nerochen@Neros-MacBook-Pro.local>
2023-10-23 16:33:40 -0700

    fix api errors for music and code generations

--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -13,8 +13,7 @@ const openai = new OpenAIApi(configuration);
 
 const instructionMessage: ChatCompletionRequestMessage = {
   role: "system",
-  content:
-    "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations.",
+  content: "You are a code generator.",
 };
 
 export async function POST(req: Request) {
--- app/api/music/route.ts
+++ app/api/music/route.ts
@@ -34,7 +34,9 @@ export async function POST(req: Request) {
       "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
       {
         input: {
+          alpha: 0.5,
           prompt_a: prompt,
+          num_inference_steps: 20,
         },
       }
     );
